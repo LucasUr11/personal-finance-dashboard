bfc2c7f3ec1c41fe704c1f2d6701fad3d759511a
Lucas Urquiza <105996657+LucasUr11@users.noreply.github.com>
2025-11-18 18:09:58 +0000

    Nuevo Home.jsx

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,30 +1,32 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import Icon from "../assets/img/Icon.jpeg"
+import home_page from "../assets/img/home_9.jpg"
 
 
 export const Home = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="home-container d-flex flex-column align-items-center text-center">
-            <div className="z-1 inicio card">
-                <h1>Gestor de gastos</h1>
-                {/* Imagen central */}
-                <div className="">
-                    <img
-                        src={Icon}
-                        alt="Logo Billetera Familiar"
-                        className=""
-                        style={{ maxWidth: "300px" }}
-                    />
-                </div>
+        <div className="home-container d-flex">
+            <div>
+                <img
+                    src={home_page}
+                    alt="home"
+                    className="home_img"
+                />
+            </div>
 
+            <div className="home_text">
+                <h1>¡Bienvenidos!</h1>
+                <p>
+                    Acá va a ir un párrafo donde se explique que es un gestor de gastos y para saber mas, haremos una visual donde se vea el porque es necesario tener uno para los tiempos en los que vivimos.
+                    A esa visual entraremos desde el boton 'leer mas' y los botones del formulario estaran en el navbar, hasta que no esten listos, se quedaran ahi para que se pueda seguir ingresando.
+                </p>
+                <button className="btn">Leer más</button>
                 {/* Botones inferiores lado a lado */}
-                <div className="my-2 d-flex justify-content-center gap-3 z-1">
+                <div className="my-2 d-flex gap-2">
                     <button
                         className="btn btn-registro"
-                        style={{ minWidth: "140px" }}
                         onClick={() => navigate("/Signup")}
                     >
                         Registrarse
@@ -32,13 +34,13 @@ export const Home = () => {
 
                     <button
                         className="btn btn-iniciar_sesion"
-                        style={{ minWidth: "140px" }}
                         onClick={() => navigate("/Login")}
                     >
                         Acceder
                     </button>
                 </div>
             </div>
+
         </div>
     );
 };
