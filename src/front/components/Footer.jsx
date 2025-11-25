1659f4a3514b5d16c04ceea80116a839ce464392
Lucas Urquiza <105996657+LucasUr11@users.noreply.github.com>
2025-11-25 03:30:31 +0000

    Cambios en los estilos Formularios y Footer

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,38 +1,27 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const Footer = () => {
+
+	const navigate = useNavigate();
 
 	return (
 		<div className="footer">
 
-			<div className="footer_h4">
-				<h4 className="">Tu familia...Tus metas... Tu billetera familiar!</h4>
+			<div className="footer_title">
+				<h4 className="">Tu familia...Tus metas... Tu Billetera Familiar!</h4>
 			</div>
 
-			<div className="footer_icons">
-				<a href="https://4geeks.com/es/login" target="_blank">
-					<i className="fa-regular fa-envelope"></i>
-				</a>
-				<a href="https://github.com/4GeeksAcademy" target="_blank">
-					<i className="fa-brands fa-github"></i>
-				</a>
-				<a href="https://www.linkedin.com/school/4geeksacademy/posts/?feedView=all" target="_blank">
-					<i className="fa-brands fa-linkedin"></i>
-				</a>
-			</div>
-
-			<div className="footer_button btn-group dropend">
-				<button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-					¿Quienes somos?
+			<div className="footer_button">
+				<button
+					type="button"
+					className="btn"
+				// navigate
+				>
+					Sobre nosotros
 				</button>
-				<ul className="dropdown-menu">
-					<li><a href="http://">Elizabeth</a></li>
-					<li><a href="http://">Javier</a></li>
-					<li><a href="http://">Lucas</a></li>
-				</ul>
 			</div>
 
-			<div className="footer_copyright py-3">
+			<div className="footer_copyright">
 				<p>©2025. Billetera Familiar SA. Todos los derechos reservados.</p>
 			</div>
 		</div>
